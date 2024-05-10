897cff53443916210c4d6bfabfa4f8221a07dc2a
MohamedkamalSoudi <mk0875734@gmail.com>
2024-05-10 22:21:01 +0300

    ScreenUtilInit has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:music_notes_player_app_setup/views/tune_view.dart';
-void main() {
-  runApp(const TuneApp());
-}
+
+void main() => runApp( 
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => TuneApp(), // Wrap your app
+  ),
+);
 
 class TuneApp extends StatelessWidget {
   const TuneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TuneView(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child:  MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        home: TuneView(),
+      ),
     );
   }
 }
