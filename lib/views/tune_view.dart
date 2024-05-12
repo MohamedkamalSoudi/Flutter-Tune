2f790a02ee5e12dfd30d6b47fb7dda408dc4a8ae
MohamedkamalSoudi <mk0875734@gmail.com>
2024-05-12 20:04:22 +0300

    container

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:music_notes_player_app_setup/widgets/tune_item.dart';
 
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
@@ -13,6 +14,31 @@ class TuneView extends StatelessWidget {
         title: Text("Flutter Tune"),
         elevation: 0,
         backgroundColor: Color(0xff253238),
+      ),
+      body: Column(
+        children: [
+          TuneItem(
+            color: Colors.red,
+          ),
+          TuneItem(
+            color: Colors.green,
+          ),
+          TuneItem(
+            color: Colors.pink,
+          ),
+          TuneItem(
+            color: Colors.purple,
+          ),
+          TuneItem(
+            color: Colors.blue,
+          ),
+          TuneItem(
+            color: Colors.black87,
+          ),
+          TuneItem(
+            color: Colors.purpleAccent,
+          ),
+        ],
       ),
     );
   }
